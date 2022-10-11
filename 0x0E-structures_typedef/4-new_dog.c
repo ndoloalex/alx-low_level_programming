dca7e08ab26c246493eb4c6ce3b77f01f7d7b210
Alex Ndolo <78944304+ndoloalex@users.noreply.github.com>
2022-10-11 20:45:09 +0300

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,14 +10,11 @@
 int _strlen(char *s)
 {
 	int i;
-
 	i = 0;
-
 	while (s[i] != '\0')
 	{
 		i++;
 	}
-
 	return (i);
 }
 
@@ -33,15 +30,12 @@ int _strlen(char *s)
 char *_strcpy(char *dest, char *src)
 {
 	int len, i;
-
 	len = 0;
-
 	while (src[len] != '\0')
 	{
 		len++;
 	}
 	for (i = 0; i < len; i++)
-	
 	{
 		dest[i] = src[i];
 	}
@@ -61,14 +55,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
 	int len1, len2;
-
 	len1 = _strlen(name);
 	len2 = _strlen(owner);
-
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-
 	dog->name = malloc(sizeof(char) * (len1 + 1));
 	if (dog->name == NULL)
 	{
@@ -82,10 +73,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(dog->name);
 		return (NULL);
 	}
-
 	_strcpy(dog->name, name);
 	_strcpy(dog->owner, owner);
 	dog->age = age;
-
 	return (dog);
 }
